b96141bd0b8ea152d34e82d9eaa6cc0abe558c53
tatiany <tatianysouza2018@gmail.com>
2024-12-14 18:52:51 -0300

    Adicionando página html

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import eventoRouter from './router/eventoRouter.js'; 
+import path from 'path';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(path.resolve(), 'views')));
 app.use('/eventos', eventoRouter); 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(path.resolve(), 'views', 'index.html'));
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
